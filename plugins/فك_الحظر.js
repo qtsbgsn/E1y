b09fe76d15c82e7ebef1d91ee36445a7071b4022
bot865 <111519198+bot865@users.noreply.github.com>
2024-07-25 00:50:39 +0300

    Update فك_الحظر.js

--- plugins/فك_الحظر.js
+++ plugins/فك_الحظر.js
@@ -1,9 +1,9 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('*❁┇تم إلغاء حظر هذه الدردشة بنجاح┇❁*')
+m.reply('*ابسط ي عم البانشات اتفك ليكم اهو قولو شكرا بقا لعمو العقرب 😂♥*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = /^فك-الحظر|unbanchat|فك-الحظر$/i
+handler.command = /^رفع-بانشات|unbanchat|فك-الحظر$/i
 handler.rowner = true
 export default handler
